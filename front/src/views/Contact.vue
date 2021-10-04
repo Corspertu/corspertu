63da7d80e998006ea97a50bdbffa2a6f54360e65
ArnaudBascou <bascou.arnaud@hotmail.fr>
2021-10-04 18:53:05 +0200

    contact menu problem

--- front/src/views/Contact.vue
+++ front/src/views/Contact.vue
@@ -11,24 +11,24 @@
       </v-img>
 
       <v-card-text class="text--primary" style="font-size: 1.2em; margin-top: 1em">
-        <div><v-icon> mdi-phone</v-icon>Téléphone: 06.20.59.02.94</div>
-        <div><v-icon> mdi-email</v-icon>contact@corspertu.corsica</div>
+        <div><v-icon> mdi-phone</v-icon> Téléphone: 06.20.59.02.94</div>
+        <div><v-icon> mdi-email</v-icon> contact@corspertu.corsica</div>
         <div>
-          <v-icon> mdi-crosshairs-gps</v-icon>Res. Les Cimes Bat. 4 Route
+          <v-icon> mdi-crosshairs-gps</v-icon> Res. Les Cimes Bat. 4 Route
           inférieure de Cardo 20200 Bastia
         </div>
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m21!1m12!1m3!1d733.1176701770102!2d9.436453982534685!3d42.693753541906595!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m6!3e0!4m3!3m2!1d42.6942405!2d9.4364017!4m0!5e0!3m2!1sfr!2sfr!4v1633364801139!5m2!1sfr!2sfr"
           width="600"
           height="450"
-          style="border: 0 ; margin-top:2em; margin-right: 5em"
+          style="border: 0 ; margin-top:2em; text-align:center"
           allowfullscreen=""
           loading="lazy"
         ></iframe>
       </v-card-text>
 
       <v-card-actions style="text-align: right">
-        <v-btn color=#3c4b65 text> <a href="mailto:contact@corspertu.corsica"></a> Envoyer un mail </v-btn>
+        <v-btn color=#3c4b65 text> <a href="mailto:contact@corspertu.corsica">Envoyer un mail</a>  </v-btn>
 
       </v-card-actions>
     </v-card>
