d34f28b55ea728872a83ed596db06acf778e551c
ArnaudBascou <bascou.arnaud@hotmail.fr>
2021-10-05 17:18:25 +0200

    legal

--- front/src/views/Legal.vue
+++ front/src/views/Legal.vue
@@ -1,32 +1,58 @@
 <template>
-   <div class="bgc">
- 
+  <div class="legal">
+    <h1>Mentions légales</h1>
 
-<h1>Mentions légales</h1>
+    <h2>Droits</h2>
+    Cors'pertu est une association de loi 1901 déclarée à la préfecture de
+    Haute-Corse le 20/04/2021. <br />
+    Le siège est situé Résidence Les Cimes Bat. 4, route inférieure de Cardo,
+    20200 Bastia <br />
+    Téléphone: 06.20.59.02.94 <br>
+    Le site est hébergé par XXXXX, au capital de XXX, immatriculée au RCS de XXX
+    sous le numéro XXX dont le siège social se trouve XXX <br />
 
-L'association Cors'pertu est enregistrée 
-    </div>
+    Responsable de publication: Arnaud Bascou
+
+    <h2>Données personnelles</h2>
+
+    Ce site a été développé dans le meilleur respect de vos données
+    personnelles. Des données techniques sont conservées dans les journaux et les cookies mais ne
+    contiennent aucune autre information que celles nécessaires à leur usage
+    technique. L'analyse du traffic du site est réalisée avec des données
+    anonymisées.
+
+    Responsable du fichier: Arnaud Bascou
+
+    <h2>Crédits Photo</h2>
+    <ul>
+      <li>Photos libres de droits</li>
+      <li>Photos personnelles du propriétaire du site</li>
+    </ul>
+
+    <h2>CGU/CGV</h2>
+    Les CGU et CGV sont remises lors de l'établissement du devis.
+  </div>
 </template>
 
 <script>
 export default {
-    setup() {
-        
-    },
-}
+  setup() {},
+};
 </script>
 
 <style>
-.bgc {
-    background-color: #9b9aa3;
+.legal {
+  padding-top: 4em;
 }
 
-h1,h2,.img {
+h1,
+h2,
+.img {
   text-align: center;
   padding: 5px;
 }
 
-h1{
+h1 {
   margin-top: 1em;
   font-weight: bolder;
   color: #3c4b65;
