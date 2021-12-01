ef01b5291580fdb96ea59c559db605e44004d080
ArnaudBascou <bascou.arnaud@hotmail.fr>
2021-12-01 15:59:42 +0100

    title blog card read

--- front/src/components/BlogThumbnail.vue
+++ front/src/components/BlogThumbnail.vue
@@ -2,7 +2,7 @@
  
   
     
-<v-col xs="12" sm="6" md="6" lg="6" xl="6">
+<v-col xs="12" sm="12" md="6" lg="6" xl="6">
   <v-card  class="cardMargin" style="margin:1em" v-if="posts">
          <v-img
           class="indigo accent--text "
@@ -10,11 +10,7 @@
           v-if="posts._embedded['wp:featuredmedia']"
           :src="posts._embedded['wp:featuredmedia'][0].source_url"
         >
-          <v-card-title style="word-break: break-word">
-            <div class="titleCard">
-              {{decoder(posts.title.rendered)}}
-            </div>
-          </v-card-title>
+          
         </v-img>
         <v-img
           class="indigo accent--text "
@@ -22,17 +18,17 @@
           v-else
           src="../assets/budgetFinance.jpg"
         >
-          <v-card-title style="word-break: break-word">
-            <div class="titleCard">
-              {{decoder(posts.title.rendered)}}
-            </div>
-          </v-card-title>
         </v-img>
 
         <v-card-text
           class="text--primary"
-          style="font-size: 1em; margin-top: 1em;"
+          style="font-size: 1em"
         >
+        <v-card-title style="word-break: break-word">
+            <div class="titleCard">
+              {{decoder(posts.title.rendered)}}
+            </div>
+          </v-card-title>
           <v-html>
              {{decoder(posts.excerpt.rendered)}}
           </v-html>
@@ -96,7 +92,7 @@ export default {
 
 .titleCard{
  font-weight: bold; 
- text-shadow: #3c4b65 0.1em 0.1em 0.2em;
+ color: #657eab;
 }
 
 
